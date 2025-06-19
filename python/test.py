660d3f59cd41d28d6b25dd64ae0ba3f3c26f8ff5
Ayan Guchhait <ayanwork45@gmail.com>
2025-06-19 08:14:41 +0530

    Add files via upload

---
+++ python/test.py
@@ -0,0 +1,12 @@
+## This is single line comment
+'''
+This is a example of multiline comments
+testing the basics
+
+''' 
+# Python is case sensitive
+# can use a back slash (\) to continue the statement to the next line
+# here also semi colons can be used - x=10;y=2;z=x+y
+# name convensions- descriptive,start with letters,underscore
+# variable names are case sensitive
+# Dynamic typing
